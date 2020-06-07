9d667b9fdcfa05c2c1dd4192fc7a05a43e0c75b9
Agustín La Valla <agulavalla@gmail.com>
2020-06-07 17:32:21 -0300

    Main Function fixed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-const {server, app }= require('./server');
+const { server, app } = require('./server');
 const { connect } = require('./db');
 const colors = require('colors');
 
 const port = process.env.PORT || 3000;
 
 async function main() {
-    await connect(); 
-    await server.listen(port);
-    console.log(`${colors.magenta('Server on port')} ${colors.green(port)}`);
+    try {
+        await connect();
+        await server.listen(port);
+        console.log(`${colors.magenta('Server on port')} ${colors.green(port)}`);
+    } catch (error) {
+        console.log(error);
+    };
 };
 
 main();
